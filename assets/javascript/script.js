a5353b99139c1fbacb2b9fde0f37546fba3c7e91
drewski <drew.howell@stacksports.com>
2020-01-07 18:11:58 -0500

    added suedo code to script file

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+// determine & display the current day
+
+
+
+// determine current time & highlight timeblock red
+
+
+
+// determine past time & highlight past timeblocks gray
+
+
+
+// determine future time & highlight past timeblocks green
+
+
+
+// input text and save in each timeblock
