b4422031ac55f441e639546efdf04ee617e82afc
drewski <drew.howell@stacksports.com>
2020-01-10 08:14:47 -0500

    got timeblocks to turn red

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 // create, determine, & display the current day
 
-var currentDay = moment().format("dddd, MMMM Do YYYY");
+var currentDay = moment().format("dddd, MMMM Do YYYY " + " @ h:mm A");
 
 function insertCurrentDay() {
     $("#current").text(currentDay);
@@ -10,9 +10,23 @@ insertCurrentDay();
 
 console.log(currentDay);
 
+console.log("hi");
+
 
 
 // determine current time & highlight timeblock red
+var time = [9,10,11,12,13,14,15,16,17]
+var currentTimeEl = moment().format("h");
+
+console.log(currentTime);
+
+function currentTime() {
+    if (currentTimeEl <= time) {
+        $(".block").css("background-color", "red");
+    }
+}
+
+currentTime();
 
 
 
